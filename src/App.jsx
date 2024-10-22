e4221e1e83a3ec5c489f26ff14f45ae00083aeb3
mindahun21 <mndahun0@gmail.com>
2024-10-22 21:18:32 +0300

    final submision

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 const App = () => {
   return (
-    <Router>
+    <Router basename="vite-deploy">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
